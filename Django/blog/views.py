837b288d9e008b58be6e226bfc73c0eaed550e29
Emmanuel Alves <emmanuelalves@locus.software>
2019-08-20 19:20:09 -0300

    Created publish flux

--- Django/blog/views.py
+++ Django/blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') 
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -42,3 +42,8 @@ def post_edit(request, pk):
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts' : posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
